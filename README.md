fdd5a83eadbcd3bdc60478516d1546b21434ba34
zhengjia <jiazheng@live.com>
2011-12-22 14:07:46 -0600

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Vagrant with rbenv installed.
---
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -0,0 +1,6 @@
+require_recipe 'apt'
+require_recipe 'gems'
+require_recipe 'git'
+require_recipe 'openssl'
+require_recipe "build-essential"
+require_recipe "sqlite"
