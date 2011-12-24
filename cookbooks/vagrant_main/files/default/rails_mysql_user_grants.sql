1819b60fd184af917dbea73caa942dfbd5fc88ab
zhengjia <jiazheng@live.com>
2011-12-23 21:42:32 -0600

    remove unused cookbooks

--- cookbooks/vagrant_main/files/default/rails_mysql_user_grants.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
--- cookbooks/vagrant_main/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-require_recipe 'apt'
-require_recipe 'gems'
-require_recipe 'git'
-require_recipe 'openssl'
-require_recipe "build-essential"
-require_recipe "sqlite"
